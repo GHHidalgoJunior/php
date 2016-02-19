54ac03339f610fdc6187ead842625e067eb184f7
hidalgo <hidalgo@Peniel>
2016-02-19 13:40:23 -0200

    Primeiro envio

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prof. Arnaldo M. Hidalgo Jr. - E-mail: arnaldo.hidalgo@etec.sp.gov.br</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/w3.css" rel="stylesheet" type="text/css"/>
+        <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+        <style>
+            .w3-tangerine {
+                font-family: 'Tangerine', serif;
+            }
+        </style>
+
+    </head>
+    <body>
+        <header class="w3-container">
+            <h1>Prof. Arnaldo Martins Hidalgo Junior.<
+        </header>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
